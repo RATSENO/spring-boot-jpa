1ecba9ca6b599499039d4defe5eedf44dd7bfb4b
ratseno <onestar1014@gmail.com>
2019-02-19 23:46:45 +0900

    JPA 프로그래밍: Query
    JPQL (HQL)
    Java Persistence Query Language / Hibernate Query Language
    데이터베이스 테이블이 아닌, 엔티티 객체 모델 기반으로 쿼리 작성.
    JPA 또는 하이버네이트가 해당 쿼리를 SQL로 변환해서 실행함.
    https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#hql
    
            TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
            List<Post> posts = query.getResultList();
    
    Criteria
    타입 세이프 쿼리
    https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#criteria
    
    CriteriaBuilder builder = entityManager.getCriteriaBuilder();
            CriteriaQuery<Post> criteria = builder.createQuery(Post.class);
            Root<Post> root = criteria.from(Post.class);
            criteria.select(root);
            List<Post> posts = entityManager.createQuery(criteria).getResultList();
    
    Native Query
    SQL 쿼리 실행하기
    https://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#sql
    
    List<Post> posts = entityManager
                    .createNativeQuery("SELECT * FROM Post", Post.class)
                    .getResultList();

--- src/main/java/me/ratseno/demospringdata/JpaRunner.java
+++ src/main/java/me/ratseno/demospringdata/JpaRunner.java
@@ -1,9 +1,16 @@
 package me.ratseno.demospringdata;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
+import org.omg.PortableServer.POA;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -16,30 +23,27 @@ public class JpaRunner implements ApplicationRunner {
 	@PersistenceContext
 	EntityManager entityManager;
 
+	/*
+	 * @Override public void run(ApplicationArguments args) throws Exception {
+	 * TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post AS p",
+	 * Post.class); List<Post> posts = query.getResultList();
+	 * posts.forEach(System.out::println); }
+	 */
+
+	/*
+	 * @Override public void run(ApplicationArguments args) throws Exception {
+	 * CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+	 * CriteriaQuery<Post> query = builder.createQuery(Post.class); Root<Post> root
+	 * = query.from(Post.class); query.select(root);
+	 * 
+	 * List<Post> posts = entityManager.createQuery(query).getResultList();
+	 * posts.forEach(System.out::println); }
+	 */
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-
-		/*
-		 * Post post = new Post(); post.setTitle("Spring Dat JPA 언제 보나...");
-		 * 
-		 * Comment comment = new Comment(); comment.setComment("빨리 보고 싶어요");
-		 * post.addComment(comment);
-		 * 
-		 * Comment comment1 = new Comment(); comment1.setComment("곧 보여드릴께요");
-		 * post.addComment(comment1);
-		 */
-
-		Session session = entityManager.unwrap(Session.class);
-		//session.save(post);
-		Post post = session.get(Post.class, 4l);
-		System.out.println("=======================================");
-		System.out.println(post.getTitle());
-		
-		post.getComments().forEach(c ->{
-			System.out.println("==============================");
-			System.out.println(c.getComment());
-			System.out.println("==============================");
-		});
+		List<Post> posts = entityManager.createNativeQuery("Select * from Post", Post.class).getResultList();
+		posts.forEach(System.out::println);
 	}
 
 }
