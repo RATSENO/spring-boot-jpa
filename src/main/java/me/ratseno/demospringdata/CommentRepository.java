b0d806a5dc2c4b6c4ac643f4e9452b587dce0838
ratseno <onestar1014@gmail.com>
2019-03-10 15:55:36 +0900

    스프링 데이터 Common: 비동기 쿼리 비동기 쿼리 @Async Future<User> findByFirstname(String firstname); @Async CompletableFuture<User> findOneByFirstname(String firstname); @Async ListenableFuture<User> findOneByLastname(String lastname); 해당 메소드를 스프링 TaskExecutor에 전달해서 별도의 쓰레드에서 실행함. Reactive랑은 다른 것임
    권장하지 않는 이유
    테스트 코드 작성이 어려움.
    코드 복잡도 증가.
    성능상 이득이 없음.
    DB 부하는 결국 같고.
    메인 쓰레드 대신 백드라운드 쓰레드가 일하는 정도의 차이.
    단, 백그라운드로 실행하고 결과를 받을 필요가 없는 작업이라면 @Async를 사용해서 응답 속도를 향상 시킬 수는 있다.

--- src/main/java/me/ratseno/demospringdata/CommentRepository.java
+++ src/main/java/me/ratseno/demospringdata/CommentRepository.java
@@ -1,12 +1,13 @@
 package me.ratseno.demospringdata;
 
 import java.util.List;
-import java.util.stream.Stream;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.util.concurrent.ListenableFuture;
 
-public interface CommentRepository extends MyRepository<Comment, Long> {
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	//List<Comment> findByCommentContainsIgnoreCaseAndLikeCountGreaterThan(String keyword , int likeCount);
 
@@ -14,5 +15,6 @@ public interface CommentRepository extends MyRepository<Comment, Long> {
 
 	//Page<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable page);
 
-	Stream<Comment> findByCommentContainsIgnoreCase(String keyword, Pageable page);
+	@Async
+	ListenableFuture<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable page);
 }
